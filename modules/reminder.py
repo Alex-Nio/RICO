d12d2a584c9d961d6876db33f7d707720a448064
Alex Nio <volpenio@gmail.com>
2022-07-24 15:08:23 +0300

    reminder filtration fix

--- modules/reminder.py
+++ modules/reminder.py
@@ -9,73 +9,94 @@ import config
 def execute_reminder_cmd(cmd: str, voice: str, new_data, counter):
     try:
         if cmd == "remind_cmd":
-            remind_str = new_data
+            remind_data_list = new_data
             remind_num = counter
-            # список ['рик', 'и', 'напомни', 'мне', 'закрыть', 'окно', 'через', 15, 'минут']
-            print(str(remind_str) + "---data")
+
+            print(str(remind_data_list) + "---data")  # список
             print(str(remind_num) + "---remind number")  # число
 
-            def reminder(data, remind_num):  # data=строка с цифрами
+            def reminder(data, remind_num):  # data = входящий голос / remind_num = число
                 while True:
-                    # Вычисляем количество минут
-                    def calc_total_time(i, flag):
-                        if flag == 1:
-                            total_time = (i * 60) * 60
-                        elif flag == 2:
-                            total_time = i * 60
-                        elif flag == 3:
-                            total_time = i
-                        return total_time
-
-                    # Фильтруем напоминание
+                    # Фильтруем из сообщения то что нужно напомнить т.е. "открыть\закрыть", "окно"
                     def filtration(data):
+                        # Убираем название алиаса из стартовой строки
                         for x in data:
                             if x in config.VA_ALIAS:
                                 data.remove(x)
-                        for x in data:
-                            if x == 'напомни' or x == 'напомнил':
-                                data.remove(x)
-                        for x in data:
-                            if x == 'мне':
-                                data.remove(x)
-                        for x in data:
-                            if x == 'и':
-                                data.remove(x)
-                        for x in data:
-                            if type(x) == int:
-                                data.remove(x)
-                        return data
+                        # Убираем ключевое слово из стартовой строки
+                        for z in data:
+                            if z in config.VA_REMID['remind_cmd']:
+                                data.remove(z)
 
-                    data = filtration(data)
+                        # Убираем лишнее
+                        if data[0] == 'и':
+                            data.remove(data[0])
 
-                    # Проверяем вид времени
+                        # Убираем лишнее
+                        for c in data:
+                            if c == 'мне':
+                                data.remove(c)
 
+                            print(str(data) + "---result data")
+
+                            # ? Если есть слово через
+                            try:
+                                stop = data.index('через')
+                                slice_object = slice(0, stop)
+                                result = data[slice_object]
+                                result = " ".join(result)
+                                return result
+                                # TODO: Если есть слово В
+                            except ValueError:
+                                result = " ".join(data)
+                                tts.va_speak(
+                                    f'Уточните через сколько вы хотите {result} повторив условие')
+                                break
+
+                    # Отфильтровываем то, что нужно напомнить
+                    # Считаем время (часы, минуты или секунды)
+                    condition = filtration(data)
+
+                    def calc_total_time(i, flag):
+                        if flag == 1:
+                            x = (i * 60) * 60
+                        elif flag == 2:
+                            x = i * 60
+                        elif flag == 3:
+                            x = i
+                        return x
+
+                    # Определяем используемое время (часы, минуты или секунды)
                     def total_time_calculation(data, remind_num):
                         for item in data:
+                            t_time = int
+                            # Если часы
                             if item in ("час", "часа"):
                                 r_time_hours = 1
                                 t_time = calc_total_time(
                                     remind_num, r_time_hours)
+                            # Если минуты
                             elif item in ("минут", "минуты"):
                                 r_time_minutes = 2
                                 t_time = calc_total_time(
                                     remind_num, r_time_minutes)
+                            # Если секунды
                             elif item in ("секунд", "секунды"):
                                 r_time_sec = 3
                                 t_time = calc_total_time(
                                     remind_num, r_time_sec)
                         return t_time
 
-                    t_time = total_time_calculation(data, remind_num)
+                    # Тут мы подсчитали время
+                    total_time = total_time_calculation(data, remind_num)
 
-                    data = data[0:-2]
-                    data = " ".join(data)
-                    # тут мы получили количество секунд
-                    print(str(data) + " last operation")
-                    # print(t_time)  # тут мы получили количество секунд
-
-                    tts.va_speak('Хорошо, запомнила')
-                    sleep_rec(t_time, data)
+                    if condition is not None:
+                        tts.va_speak('Хорошо, запомнила')
+                        sleep_rec(total_time, condition)
+                        break
+                    else:
+                        pass
+                        break
 
             def sleep_rec(t_time, data):
                 # Запуск таймера напоминания в потоке
@@ -84,11 +105,10 @@ def execute_reminder_cmd(cmd: str, voice: str, new_data, counter):
                 t_time = num2text(t_time)
                 tts.va_speak(
                     f'Напоминаю, нужно {data}, прошло {t_time} секунд')
-
             #! Тут производим передачу даты и запуск
             # # Создаём новый поток
             th = Thread(target=reminder, daemon=True,
-                        args=(remind_str, remind_num,))
+                        args=(remind_data_list, remind_num,))
             th.start()
      # ? Обработка ошибки если не выполнен запуск программы по ключевым словам
     except NameError:
