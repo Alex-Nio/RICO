ab212048a9250d797f9d4093189e3b1fddf8e761
Alex Nio <volpenio@gmail.com>
2022-07-26 11:12:53 +0300

    add base for WF functions and code teory

--- modules/workflow_commands.py
+++ modules/workflow_commands.py
@@ -1,10 +1,12 @@
 from modules import tts  # pylint: disable=ungrouped-imports
+from modules import workflow_functions as wf
 
 # ? Напоминание
 def execute_workflow_cmd(cmd: str, voice: str, new_data, counter):
     try:
         if cmd == "create_workflow":
             print("Команда create_workflow выполнена успешно!")
+            wf.myfunc()
     # ? Обработка ошибки если не выполнен запуск программы по ключевым словам
     except NameError:
         tts.va_speak("Произошла ошибка во время выполнения команды")
